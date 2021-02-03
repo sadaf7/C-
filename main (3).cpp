e1ae4373fa1dd66806252a61fc83eb83ba1c307f
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-03 12:45:01 +0600

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ string f = "I am sadaf hossain";
+ cout <<f <<endl;
+ int C = 13;
+ cout << "My age is :" << C;
+   
+
+    return 0;
+}
