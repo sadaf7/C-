fc5168d31beb3eb91767b2379c737e98c5395006
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-04 12:12:19 +0600

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float X = 2.5;
+    
+    double C = 22.54891247;
+    
+     long double sum = X + C;
+    
+    cout << sum << endl;
+    
+    long Y = 4512781;
+    
+    short R = -500;
+    
+    long sub = Y - R;
+    
+    cout << sub;
+    
+    
+    return 0;
+    
+}
