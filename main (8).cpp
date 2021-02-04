d1a4b4d7caffec31e22e7342f2c8925bab39e916
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-04 13:32:27 +0600

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int X;
+  
+  char Y;
+  
+  cout << "what is the value of X and Y";
+  
+  cin >> X >> Y;
+  
+  cout << "the value of X : " << X << endl;
+  
+  cout << "the value of Y : " << Y;
+ 
+
+    return 0;
+}
