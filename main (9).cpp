fa35acc10f6ae91918c2233505c00671c7dc3974
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-04 23:31:55 +0600

    Add files via upload

---
+++ main (9).cpp
@@ -0,0 +1,23 @@
+
+ // implicit type conversion
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // integer = double 
+    
+   int X = 15;
+    
+    double F;
+    
+    F = X ;
+    
+     cout << "integer" << X << endl;
+     
+     cout << "double_num"  << F ;
+     
+    return 0;
+}
