56cdab61406500f1dd44c204289e49e9ff43e028
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-04 13:06:57 +0600

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int num;
+  
+  cout << "enter an integer";
+  
+  cin >> num;
+  
+  cout << "and the number is : " << num;
+  
+    return 0;
+}
