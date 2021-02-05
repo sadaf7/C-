34fb64b135c3e8df8307f91b666b22e37c66e3bd
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-05 11:32:43 +0600

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,23 @@
+// implicit conversion
+
+#include <iostream>
+
+using namespace std;
+
+// double value to integer
+
+int main()
+{
+    int F;
+    
+    double X = 10.56;
+    
+    F = X ;
+    
+    cout << " integr " << F << endl;
+    
+    cout << "dnum" << X ; 
+
+    
+    return 0;
+}
