ccf1f5baf0d9655034c4528187858f1283ecf840
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-05 11:42:08 +0600

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,23 @@
+// explicit conversion
+
+#include <iostream>
+
+using namespace std;
+
+// type casting
+
+int main()
+{
+   
+    double F = 7.45;
+    
+    int X ;
+    
+    X = (double) F;
+    
+    cout << "dnum" << F ;
+    
+    cout << " intgr " << X ;
+    
+    return 0;
+}
