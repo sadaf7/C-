0de1e6a83afd6002ac912eb43a3da3db3bb9949d
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-09 14:59:39 +0600

    Add files via upload

---
+++ main (14).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+     
+     cout << "enter a number : " << endl;
+     
+     cin >> num;
+     
+     if(num != 0)
+     {
+         if((num % 2) == 0){
+            
+         
+          
+          cout << "the number is even" << endl;
+     }
+     
+        else {
+            
+            cout << "the number is odd"  <<endl;
+        }
+         
+         
+     }
+        
+        
+        
+        
+    return 0;
+}
