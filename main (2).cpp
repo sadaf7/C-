67d8a0ecb959d59032ff6cc6b64e45d279f5683e
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-19 11:31:26 -0800

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int X = 45;
+    int Y = 54;
+    
+    int sadaf = ++X;
+    int hossain = --Y;
+    
+    cout << "sdaf : " << sadaf << endl;
+    cout << "hossain : " << hossain << endl;
+    
+    return 0;
+    
+}
